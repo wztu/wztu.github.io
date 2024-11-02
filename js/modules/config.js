04a369cf42483b53033e41187bc22374f55ba78f
wztu <wz1080p@gmail.com>
2024-11-02 16:34:38 +0800

    Update config.js

--- js/modules/config.js
+++ js/modules/config.js
@@ -3,7 +3,7 @@ export default {
   token: "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",
   userAndRepo: "wztu/wztu.github.io",
   branch: "master",
-  path: "/img",
+  path: "/tu",
   dns: "",
   ...JSON.parse(localStorage.getItem("config")) // 加载配置
 }
