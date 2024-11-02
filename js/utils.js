f3d793a97338b976f7f9f8c7e8c9d283b90489ab
tvwz <wuzhijj@gmail.com>
2024-11-02 08:42:48 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 function urlFormat(url, format = "md", describe = new Date().Format('yyyy-MM-dd')) {
   switch (format) {
     case "md":
-      return `![](${url})`
+      return `${url}`
       break
     default:
       return url
